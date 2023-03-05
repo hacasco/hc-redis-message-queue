314aa05189e82e35a7ac2ed17d19f50b6597a20d
Hugo Casco <hugo.casco@maigio.com>
2023-03-05 11:32:47 -0300

    Add controller.

---
+++ RedisMessageQueue/RedisMessageQueue/Controllers/QueueController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisMessageQueue.Domain.Interfaces;
+
+namespace RedisMessageQueue.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QueueController : ControllerBase
+    {
+        public QueueController(IQueueRepository queueRepository)
+        {
+            _queueRepository = queueRepository;
+        }
+
+        public async Task<IActionResult> GetAllMessages()
+        {
+            return Ok(_queueRepository.GetAllMessagesAsync());
+        }
+
+        private IQueueRepository _queueRepository;
+    }
+}
